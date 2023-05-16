cb0aba13ddd2f1633f4cbe809711af491feac49d
Paolo Landeros <landerospaolo@gmail.com>
2023-05-15 21:10:36 -0400

    changes port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Servidor funcionando en http://localhost:${PORT}`));
 
